ab308888038f7f11580036e0a22c638c54fa554d
Matheus Alves <mathalves07.winstore@outlook.com>
2023-08-26 20:14:33 -0300

    Introduction to context API

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 
 //Components
@@ -6,6 +6,19 @@ import FirstComponent from './Components/FirstComponent';
 import SecondComponent from './Components/SecondComponent';
 import Destructuring, { Category } from './Components/Destructuring';
 import State from './Components/State';
+import Context from './Components/Context';
+
+//Type
+//type textOrNull = string | null;
+
+//Interfaces
+interface IAppContext {
+  language: string,
+  framework: string,
+  projects: number
+}
+
+export const AppContext = createContext<IAppContext|null>(null);
 
 function App() {
   //Variaveis
@@ -18,25 +31,41 @@ function App() {
     return `Hey, ${name}! How r u doing?`;
   }
 
-  return (
-    <div className="App">
-      <h1>Typescript with React</h1>
-      <h2>
-        My name is: {name}, I'm {age} years old, and {isWorking ? "I'm currently working" : "I'm not currently working"}
-      </h2>
-      <h3>{userGreeting(name)}</h3>
+  //Type
+  /*const myText: textOrNull = "There's something here.";
+  
+  let secondText: textOrNull = null;
+  secondText = "Boyah";*/
 
-      <FirstComponent/>
-      <SecondComponent name="Test"/>
-      <Destructuring
-        title="First post"
-        content="Some content"
-        commentsQty={10}
-        tags={["ts","react","first-project","example"]}
-        category={Category.TS}
-      />
-      <State/>
-    </div>
+//Context
+const contextValue: IAppContext = {
+  language: "Javascript",
+  framework: "Express",
+  projects: 5,
+}
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>Typescript with React</h1>
+        <h2>
+          My name is: {name}, I'm {age} years old, and {isWorking ? "I'm currently working" : "I'm not currently working"}
+        </h2>
+        <h3>{userGreeting(name)}</h3>
+
+        <FirstComponent/>
+        <SecondComponent name="Test"/>
+        <Destructuring
+          title="First post"
+          content="Some content"
+          commentsQty={10}
+          tags={["ts","react","first-project","example"]}
+          category={Category.TS}
+        />
+        <State/>
+        <Context></Context>
+      </div>
+    </AppContext.Provider>
   );
 }
 
