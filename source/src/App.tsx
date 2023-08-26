9a4fe5c53e351d04af975b00292a9337ec3baa43
Matheus Alves <mathalves07.winstore@outlook.com>
2023-08-26 13:50:18 -0300

    Created the first component in the project

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import FirstComponent from './Components/FirstComponent';
+
 function App() {
+  //Variaveis
+  const name: string = "John Doe";
+  const age: number = 30;
+  const isWorking: boolean = true;
+
+  //Funcoes
+  const userGreeting = (name: string): string => {
+    return `Hey, ${name}! How r u doing?`;
+  }
+
   return (
     <div className="App">
       <h1>Typescript with React</h1>
+      <h2>
+        My name is: {name}, I'm {age} years old, and {isWorking ? "I'm currently working" : "I'm not currently working"}
+      </h2>
+      <h3>{userGreeting(name)}</h3>
+
+      <FirstComponent/>
     </div>
   );
 }
---
+++ source/src/Components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+//Components
+import React, { ReactElement } from 'react';
+
+function FirstComponent(): ReactElement {
+    return (
+        <div>
+            <h1>My first component</h1>
+        </div>
+    );
+}
+
+export default FirstComponent;
